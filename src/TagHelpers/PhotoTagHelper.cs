7354e350213587f1c7d71cdac507580b74a44100
Mads Kristensen <post@madskristensen.net>
2017-07-31 10:28:12 -0700

    Minor tweaks

--- src/TagHelpers/PhotoTagHelper.cs
+++ src/TagHelpers/PhotoTagHelper.cs
@@ -24,15 +24,20 @@ namespace PhotoGallery.TagHelpers
             output.Attributes.Add("alt", Photo.DisplayName);
             output.Attributes.Add("src", thumbnail);
             output.Attributes.Add("class", "lazy");
+            output.Attributes.Add("srcset", "/img/_.gif");
 
             if (Type != ImageType.Full)
             {
                 // This is for lazy loading in album view
-                output.Attributes.Add("srcset", "/img/_.gif");
 
                 string thumb = Photo.GetThumbnailLink((int)ImageType.Thumbnail, out int thumbHeight);
                 string cover = Photo.GetThumbnailLink((int)ImageType.Cover, out int coverHeight);
                 output.Attributes.Add("data-srcset", $"{thumb} {(int)ImageType.Thumbnail}w, {cover} {(int)ImageType.Cover}w");
+            }
+            else
+            {
+                string cover = Photo.GetThumbnailLink((int)ImageType.Cover, out int coverHeight);
+                output.Attributes.Add("data-srcset", $"{cover} {(int)ImageType.Cover}w, {thumbnail} {(int)Type}w");
             }
         }
     }
