b9dadc0731b33dc69be64a7afeb624e7bf0a562b
Mads Kristensen <post@madskristensen.net>
2017-08-02 10:38:00 -0700

    Variuos fixes

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,12 +1,11 @@
 ﻿(function () {
 
     var keyMap = {
-        "36": document.querySelector("link[rel=index]"), // home
         "37": document.querySelector("a[rel=prev]"), // left
         "39": document.querySelector("a[rel=next]") // right
     };
 
-    window.addEventListener("keypress", function (e) {
+    window.addEventListener("keyup", function (e) {
         if (e.altKey || e.shiftKey || e.ctrlKey)
             return;
 
