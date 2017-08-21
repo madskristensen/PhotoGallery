c3a8220a1a6fd93e671f49a43074641745bd02a1
Mads Kristensen <post@madskristensen.net>
2017-08-21 08:39:31 -0700

    Fixed validation and a11y issues

--- src/Pages/Login.cshtml.cs
+++ src/Pages/Login.cshtml.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
